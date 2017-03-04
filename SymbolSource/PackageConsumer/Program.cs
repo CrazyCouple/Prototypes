9babe66f79db7274c8fd517143121fba096aeec6
shogun <iivchenko@live.com>
2017-03-04 18:34:11 +0200

    Add Symbol and Server Test Prototype

---
+++ SymbolSource/PackageConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using TestPackage;
+
+namespace PackageConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestClass();
+
+            test.TestMethod("Hello");
+        }
+    }
+}
---
+++ SymbolSource/TestPackage/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestPackage
+{
+    public class TestClass
+    {
+        public void TestMethod(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                TestMethodPrivate("The string is empty!!!");
+            }
+            else
+            {
+                TestMethodPrivate(input);
+            }
+        }
+
+        private static void TestMethodPrivate(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
